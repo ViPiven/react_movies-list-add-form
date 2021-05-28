b9d6524e0e9706b7e47235cd7358bc1be14d6561
ViPiven <vitalijjpiven777@gmail.com>
2021-05-28 16:29:51 +0300

    Updates

---
+++ precache-manifest.be56bb5b8b2646e31c09c3c67725cdde.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "439121a7070fd406e9ba53c740f906d7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2a317b9e2df72aaec003",
+    "url": "./static/css/2.f6a27176.chunk.css"
+  },
+  {
+    "revision": "3010eb580c0302830468",
+    "url": "./static/css/main.3a7af235.chunk.css"
+  },
+  {
+    "revision": "2a317b9e2df72aaec003",
+    "url": "./static/js/2.347c6960.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.347c6960.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3010eb580c0302830468",
+    "url": "./static/js/main.c90bcaff.chunk.js"
+  },
+  {
+    "revision": "3cfb45b61c015a055433",
+    "url": "./static/js/runtime-main.0527da25.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2088c32903613710f8f9678a01b40cd4.js"
+  "./precache-manifest.be56bb5b8b2646e31c09c3c67725cdde.js"
 );
 
 self.addEventListener('message', (event) => {
